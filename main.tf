b252e6f9cc6b6a14cb3f7b5ebaffed3bf750c44f
Sean Sundberg <seansund@us.ibm.com>
2021-06-23 15:10:08 -0500

    Bumps git-repo to v1.2.1 and outputs repo urls
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 module "gitops-repo" {
-  source = "github.com/cloud-native-toolkit/terraform-tools-git-repo.git?ref=v1.2.0"
+  source = "github.com/cloud-native-toolkit/terraform-tools-git-repo.git?ref=v1.2.1"
 
   host  = var.host
   type  = var.type
