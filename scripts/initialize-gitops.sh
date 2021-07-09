7e0d98732334a8b0032216e2acfcbcfd6f3b96c6
Sean Sundberg <seansund@us.ibm.com>
2021-07-08 19:14:38 -0500

    Adds gitops console-link and top banner notification (#9)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/initialize-gitops.sh
+++ scripts/initialize-gitops.sh
@@ -3,6 +3,7 @@
 set -e
 
 SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
+CONFIG_DIR=$(cd "${SCRIPT_DIR}/../config"; pwd -P)
 TEMPLATE_DIR=$(cd "${SCRIPT_DIR}/../template"; pwd -P)
 BIN_DIR=$(cd "${SCRIPT_DIR}/../bin"; pwd -P)
 
@@ -10,6 +11,8 @@ YQ=$(command -v yq || command -v "${BIN_DIR}/yq")
 
 REPO="$1"
 NAMESPACE="$2"
+BANNER_LABEL="$3"
+BANNER_COLOR="$4"
 
 REPO_URL="https://${REPO}"
 
@@ -32,6 +35,36 @@ cat argocd/0-bootstrap/bootstrap/values.yaml | \
   "${YQ}" w - 'global.targetRevision' "${BRANCH}" | \
   "${YQ}" w - 'global.targetNamespace' "${NAMESPACE}" > newvalues.yaml
 cp newvalues.yaml argocd/0-bootstrap/bootstrap/values.yaml && rm newvalues.yaml
+
+CLUSTER_DIR="payload/1-infrastructure/cluster"
+mkdir -p ${CLUSTER_DIR}
+
+cat > "argocd/1-infrastructure/active/cluster.yaml" <<EOL
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: cluster-${BRANCH}
+spec:
+  destination:
+    namespace: ${NAMESPACE}
+    server: "https://kubernetes.default.svc"
+  project: ${PROJECT}
+  source:
+    path: ${CLUSTER_DIR}
+    repoURL: https://${APPLICATION_REPO}
+    targetRevision: ${BRANCH}
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+EOL
+
+cat "${CONFIG_DIR}/console-notification-top.yaml" | \
+  "${YQ}" w - 'spec.color' "${BANNER_COLOR}" | \
+  "${YQ}" w - 'spec.text' "${BANNER_LABEL}" > "${CLUSTER_DIR}/console-notification-top.yaml"
+
+cat "${CONFIG_DIR}/console-link-gitops.yaml" | \
+  "${YQ}" w - 'spec.href' "https://${APPLICATION_REPO}" > "${CLUSTER_DIR}/console-link-gitops.yaml"
 
 git add .
 git commit -m "Populates initial gitops structure"
