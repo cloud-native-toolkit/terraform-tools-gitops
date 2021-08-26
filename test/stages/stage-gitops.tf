08995bc6ed2475485c07dd6e4ce3dd696649ffec
Sean Sundberg <seansund@us.ibm.com>
2021-08-26 12:11:06 -0500

    Writes bin_dir value to .bindir
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage-gitops.tf
+++ test/stages/stage-gitops.tf
@@ -11,6 +11,10 @@ module "gitops" {
   sealed_secrets_cert = module.cert.cert
 }
 
+module setup_clis {
+  source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
+}
+
 resource null_resource gitops_output {
   provisioner "local-exec" {
     command = "echo -n '${module.gitops.config_repo}' > git_repo"
@@ -18,5 +22,9 @@ resource null_resource gitops_output {
 
   provisioner "local-exec" {
     command = "echo -n '${module.gitops.config_token}' > git_token"
+  }
+
+  provisioner "local-exec" {
+    command = "echo -n '${module.setup_clis.bin_dir}' > .bindir"
   }
 }
