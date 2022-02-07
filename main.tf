f84742b205af049af20a7bf8db3f3402fd2a943b
Sean Sundberg <seansund@us.ibm.com>
2022-02-07 02:22:42 -0600

    Bumps gitops-repo module to 1.5.0 to handle clis better
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ module setup_clis {
 }
 
 module "gitops-repo" {
-  source = "github.com/cloud-native-toolkit/terraform-tools-git-repo.git?ref=v1.4.1"
+  source = "github.com/cloud-native-toolkit/terraform-tools-git-repo.git?ref=v1.5.0"
 
   host  = var.host
   type  = var.type
