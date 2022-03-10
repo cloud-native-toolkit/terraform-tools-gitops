b134b626ba848740d7d00949b3b2b85a7f7b2e3a
Sean Sundberg <seansund@us.ibm.com>
2022-03-09 23:39:22 -0600

    Updates git repo to 1.6.1 (#48)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ module setup_clis {
 }
 
 module "gitops-repo" {
-  source = "github.com/cloud-native-toolkit/terraform-tools-git-repo.git?ref=v1.6.0"
+  source = "github.com/cloud-native-toolkit/terraform-tools-git-repo.git?ref=v1.6.1"
 
   host  = var.host
   type  = var.type
