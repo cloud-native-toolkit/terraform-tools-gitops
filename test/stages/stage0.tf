1097489992829ed52730c290413b87488b9b0304
Sean Sundberg <seansund@us.ibm.com>
2022-10-01 23:55:05 -0500

    Sets ca_cert on the gitops provider
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage0.tf
+++ test/stages/stage0.tf
@@ -1,2 +1,19 @@
 terraform {
+  required_providers {
+    gitops = {
+      source = "cloud-native-toolkit/gitops"
+    }
+    clis = {
+      source = "cloud-native-toolkit/clis"
+    }
+  }
+}
+
+data clis_check clis {
+}
+
+provider gitops {
+  bin_dir = data.clis_check.clis.bin_dir
+
+  ca_cert = var.ca_cert_file != "" ? file(var.ca_cert_file) : ""
 }
