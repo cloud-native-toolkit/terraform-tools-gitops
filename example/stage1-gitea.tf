afc8af1617b739e44fb1527c29c2ac155dfe0ccb
Sean Sundberg <seansund@us.ibm.com>
2023-06-20 12:08:58 -0500

    Fix gitea test module config
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- example/stage1-gitea.tf
+++ example/stage1-gitea.tf
@@ -2,7 +2,8 @@ module "gitea" {
   source = "github.com/cloud-native-toolkit/terraform-tools-gitea"
 
   cluster_config_file = module.cluster.config_file_path
-  olm_namespace       = module.dev_software_olm.olm_namespace
-  operator_namespace  = module.dev_software_olm.target_namespace
   instance_namespace  = module.dev_tools_namespace.name
+  ingress_subdomain   = module.cluster.platform.ingress
+  tls_secret_name     = module.cluster.platform.tls_secret
+  preserve_volumes    = false
 }
